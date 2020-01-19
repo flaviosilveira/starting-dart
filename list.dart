8a230342741f01130b32320ad1e4a87cc96ae1bd
Flávio Silveira <flavioaugustosilveira@gmail.com>
2020-01-19 09:36:04 -0300

    Lists and Maps

---
+++ list.dart
@@ -0,0 +1,15 @@
+void main(){
+  List numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8
+  ];
+
+  List allTypes = [
+    123, 'Name', true, 45.6
+  ];
+
+  print(numbers);
+  print(numbers.length);
+  print(numbers[0]);
+
+  print(allTypes[2]);
+}
---
+++ map.dart
@@ -0,0 +1,14 @@
+void main() {
+  Map user = {
+    'name': 'Me',
+    'age': 123,
+    'city_id': 45,
+    'status': true,
+    30: 'thirty'
+  };
+
+  print(user);
+  print(user.length);
+  print(user['status']);
+  print(user[30]);
+}
