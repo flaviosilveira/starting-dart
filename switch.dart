cc0efd93af0917fdc8019746d0fa64bac1af8e2c
Flávio Silveira <flavioaugustosilveira@gmail.com>
2020-01-20 11:16:02 -0300

    Switch structure

---
+++ switch.dart
@@ -0,0 +1,18 @@
+void main() {
+  // No secrets here also
+  String action = 'a';
+
+  switch(action){
+    case 'a':
+      print('a action');
+      break;
+    case 'b':
+      print('b action');
+      break;
+    case 'c':
+      print('c action');
+      break;
+    default:
+      print('default');
+  }
+}
